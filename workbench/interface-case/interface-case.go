6f6a89dda1e35e4d85a3a131524aed94f6c8efe2
aggresss <aggresss@163.com>
2020-02-08 17:39:31 +0800

    2020-02-08 17:39:31 CST W05D6

--- workbench/interface-case/interface-case.go
+++ workbench/interface-case/interface-case.go
@@ -7,41 +7,44 @@ package main
 
 import "fmt"
 
+// Human struct
 type Human struct {
 	name  string
 	age   int
 	phone string
 }
 
+// Student struct
 type Student struct {
 	Human  //匿名字段
 	school string
 	loan   float32
 }
 
+// Employee struct
 type Employee struct {
 	Human   //匿名字段
 	company string
 	money   float32
 }
 
-//Human实现SayHi方法
+//SayHi Human实现SayHi方法
 func (h Human) SayHi() {
 	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
 }
 
-//Human实现Sing方法
+// Sing Human实现Sing方法
 func (h Human) Sing(lyrics string) {
 	fmt.Println("La la la la...", lyrics)
 }
 
-//Employee重载Human的SayHi方法
+// SayHi Employee重载Human的SayHi方法
 func (e Employee) SayHi() {
 	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name,
 		e.company, e.phone)
 }
 
-// Interface Men被Human,Student和Employee实现
+// Men Interface Men被Human,Student和Employee实现
 // 因为这三个类型都实现了这两个方法
 type Men interface {
 	SayHi()
