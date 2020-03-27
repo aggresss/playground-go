00098d5c7bb3cd2bc54ba8f245a27abad3877e24
aggresss <aggresss@163.com>
2020-03-27 20:11:31 +0800

    2020-03-27 20:11:31 CST W12D5

---
+++ workbench/defer-panic-recover/demo.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	defer func() { // 必须要先声明defer，否则不能捕获到panic异常
+		fmt.Println("c")
+		if err := recover(); err != nil {
+			fmt.Println(err) // 这里的err是panic传入的内容
+		}
+		fmt.Println("d")
+	}()
+
+	f()
+}
+
+func f() {
+	fmt.Println("a")
+	fmt.Println("b")
+	panic("f")
+	fmt.Println("e")
+}
