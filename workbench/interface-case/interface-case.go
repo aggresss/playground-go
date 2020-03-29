db54d2ee76e59fded2f3bd3ef5d536866514fc65
aggresss <aggresss@163.com>
2020-03-29 14:39:14 +0800

    2020-03-29 14:39:14 CST W12D7

--- workbench/interface-case/interface-case.go
+++ workbench/interface-case/interface-case.go
@@ -7,11 +7,27 @@ package main
 
 import "fmt"
 
+// Men interface
+type Men interface {
+	SayHi()
+	Sing(lyrics string)
+}
+
 // Human struct
 type Human struct {
 	name  string
 	age   int
 	phone string
+}
+
+//SayHi Human实现SayHi方法
+func (h Human) SayHi() {
+	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
+}
+
+// Sing Human实现Sing方法
+func (h Human) Sing(lyrics string) {
+	fmt.Println("La la la la...", lyrics)
 }
 
 // Student struct
@@ -28,27 +44,22 @@ type Employee struct {
 	money   float32
 }
 
-//SayHi Human实现SayHi方法
-func (h Human) SayHi() {
-	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
-}
-
-// Sing Human实现Sing方法
-func (h Human) Sing(lyrics string) {
-	fmt.Println("La la la la...", lyrics)
-}
-
 // SayHi Employee重载Human的SayHi方法
 func (e Employee) SayHi() {
 	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name,
 		e.company, e.phone)
 }
 
-// Men Interface Men被Human,Student和Employee实现
-// 因为这三个类型都实现了这两个方法
-type Men interface {
-	SayHi()
-	Sing(lyrics string)
+// Lawyer struct
+type Lawyer struct {
+	office string
+	level  int
+	Men
+}
+
+// SayHi lawyer say
+func (l Lawyer) SayHi() {
+	fmt.Printf("Hi, I an a lawyer, I work at %s, my level is: %d\n", l.office, l.level)
 }
 
 func main() {
@@ -56,6 +67,7 @@ func main() {
 	paul := Student{Human{"Paul", 26, "111-222-XXX"}, "Harvard", 100}
 	sam := Employee{Human{"Sam", 36, "444-222-XXX"}, "Golang Inc.", 1000}
 	tom := Employee{Human{"Tom", 37, "222-444-XXX"}, "Things Ltd.", 5000}
+	alex := Lawyer{"TooExpensiveOffice", 3, nil}
 
 	//定义Men类型的变量i
 	var i Men
@@ -77,7 +89,7 @@ func main() {
 	// x := make([]Men, 3)
 	//这三个都是不同类型的元素，但是他们实现了interface同一个接口
 	// x[0], x[1], x[2] = paul, sam, mike
-	x := [...]Men{paul, sam, mike}
+	x := [...]Men{paul, sam, mike, tom, alex}
 
 	for _, value := range x {
 		value.SayHi()
