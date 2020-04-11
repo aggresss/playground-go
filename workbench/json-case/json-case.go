928a3cf875b2c658035a45a37808128744c99dae
aggresss <aggresss@163.com>
2020-04-11 18:50:09 +0800

    2020-04-11 18:50:09 CST W14D6

--- workbench/json-case/json-case.go
+++ workbench/json-case/json-case.go
@@ -1,10 +1,15 @@
+// https://www.jianshu.com/p/31757e530144
+
 package main
 
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"strings"
 )
 
+// Product for test json
 type Product struct {
 	Name      string  `json:"name"`
 	ProductID int64   `json:"product_id,string"`
@@ -15,11 +20,24 @@ type Product struct {
 }
 
 func main() {
-	var data = `{"name":"Xiao mi 6","product_id":"10","number":"10000","price":"2499","is_on_sale":"true"}`
-	p := &Product{}
-	err := json.Unmarshal([]byte(data), p)
-	if err != nil {
-		fmt.Println(err)
+	var data = `{
+		"name":"Xiao mi 6",
+		"product_id":"10",
+		"number":"10000",
+		"price":"2499",
+		"is_on_sale":"true"
+		}`
+
+	p1 := &Product{}
+	if err1 := json.Unmarshal([]byte(data), p1); err1 != nil {
+		fmt.Println(err1)
 	}
-	fmt.Println(*p)
+	fmt.Printf("%#v\n", p1)
+
+	p2 := new(Product)
+	if err2 := json.NewDecoder(strings.NewReader(data)).Decode(p2); err2 != nil {
+		log.Fatalln(err2)
+	}
+	fmt.Printf("%#v\n", p2)
+
 }
