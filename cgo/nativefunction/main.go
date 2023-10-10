d652e10c4f1470caafa8e6d4e883faa7b22a2bc9
aggresss <aggresss@163.com>
2023-10-10 09:19:07 +0800

    2023-10-10 09:19:07 CST W41D2

--- cgo/nativefunction/main.go
+++ cgo/nativefunction/main.go
@@ -3,12 +3,17 @@ package main
 /*
 // preamble
 # include <stdio.h>
+# include <stdint.h>
+# include <stdlib.h>
 # include <errno.h>
 
 int Add(int a, int b) {
 	errno = 1;
 	return a+b;
 }
+
+uint8_t buf[10] = {1,2,3,4,5,6,7,8,9,0};
+
 */
 import "C"
 import (
@@ -27,9 +32,27 @@ func dumpArray(array *C.uchar, length int) []byte {
 	return buf
 }
 
+// Convert *C.uchar to []byte
+// use the memory directly without a copy.
+func cuchar2Bytes(buf *uint8, size int) []byte {
+	// https://pkg.go.dev/unsafe#Slice >=1.17
+	return unsafe.Slice(buf, size)
+
+	// https://pkg.go.dev/reflect#SliceHeader <1.17
+	// s := reflect.SliceHeader{
+	// 	Data: uintptr(unsafe.Pointer(buf)),
+	// 	Len:  size,
+	// 	Cap:  size,
+	// }
+	// return *((*[]byte)(unsafe.Pointer(&s)))
+
+}
+
 func main() {
 	c, err := C.Add(C.int(1), C.int(2))
 	fmt.Println(c, err)
 
 	fmt.Println(os.Getenv("PKG_CONFIG_PATH"))
+
+	fmt.Println(cuchar2Bytes((*uint8)(unsafe.Pointer(&C.buf)), 10))
 }
