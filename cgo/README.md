b1759689142d5795be2b7bbea53b60718b4f8522
aggresss <aggresss@163.com>
2023-10-13 22:19:45 +0800

    2023-10-13 22:19:45 CST W41D5

--- cgo/README.md
+++ cgo/README.md
@@ -7,5 +7,12 @@
 
 - [Go 与 C 的桥梁：cgo 入门，剖析与实践](https://zhuanlan.zhihu.com/p/349197066)
 - [Statically Linking Go in 2022](https://mt165.co.uk/blog/static-link-go/)
+- [Hidden Dragons of CGO: Hard-Learned Lessons from Writing Go Wrappers](https://docs.yottadb.com/Presentations/DragonsofCGO.pdf)
+-
 
 通过 import “C” 语句启用 CGO 特性后，CGO 会将上一行代码所处注释块的内容视为 C 代码块，被称为序文（preamble）。
+
+C 访问 Go 函数
+- c 中 declare
+- go 中 definition 并 //export <func_name>
+- 使用 (C.type)(C.<func_name>)
---
+++ cgo/struct-field/main.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct {
+	char *msg;
+} myStruct;
+
+void myFunc(myStruct *st)
+{
+	printf("Hello %s!\n", st->msg);
+}
+*/
+import "C"
+import (
+	"runtime"
+	"unsafe"
+)
+
+func main() {
+	st := C.myStruct{C.CString("world")}
+	runtime.SetFinalizer(&st, func(t *C.myStruct) {
+		C.free(unsafe.Pointer(t.msg))
+	})
+	C.myFunc(&st)
+}
+
+// panic: runtime error: cgo argument has Go pointer to unpinned Go pointer
