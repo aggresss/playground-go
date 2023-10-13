576690bd4de3aa1bdcd575fc3f2193235f651477
aggresss <aggresss@163.com>
2023-10-13 11:25:19 +0800

    2023-10-13 11:25:19 CST W41D5

--- cgo/callback/main.go
+++ cgo/callback/main.go
@@ -8,11 +8,11 @@ package main
 
 typedef int (*callback)(void*, char*, int);
 
-static void call_later(int delay, callback cb) {
+static void call_later(int delay, callback cb, void* data) {
   sleep(delay);
   char* alpha = "beta";
   int gamma = 5;
-  cb(NULL, alpha, gamma);
+  cb(data, alpha, gamma);
 }
 
 int go_cb(void*, char*, int);
@@ -20,16 +20,28 @@ int go_cb(void*, char*, int);
 */
 import "C"
 import (
+	"errors"
 	"fmt"
 	"unsafe"
 )
 
+type LocalCallback C.callback
+
+// faild: type LocalImitate func(unsafe.Pointer, *C.char, C.int) C.int
+
+// panic: runtime error: cgo argument has Go pointer to unpinned Go pointer
+
+// but stack vavirant address ok.
+
 func main() {
-	C.call_later(3, C.callback(C.go_cb))
+	data := errors.New("omicron")
+	C.call_later(1, C.callback(C.go_cb), Save(&data))
+	// ok: C.call_later(3, LocalCallback(C.go_cb))
 }
 
 //export go_cb
 func go_cb(a unsafe.Pointer, b *C.char, c C.int) C.int {
-	fmt.Println(a, b, c)
+	f := (Restore(a)).(*error)
+	fmt.Println(*f, b, c)
 	return 0
 }
---
+++ cgo/callback/pointer.go
@@ -0,0 +1,42 @@
+package main
+
+/*
+#include <stdlib.h>
+*/
+import "C"
+import (
+	"sync"
+	"unsafe"
+)
+
+var (
+	m sync.Map
+)
+
+func Save(v interface{}) unsafe.Pointer {
+	if v == nil {
+		return nil
+	}
+	var ptr unsafe.Pointer = C.malloc(C.size_t(1))
+	if ptr == nil {
+		panic("can't allocate 'cgo-pointer hack index pointer': ptr == nil")
+	}
+	m.Store(ptr, v)
+	return ptr
+}
+
+func Restore(ptr unsafe.Pointer) (v interface{}) {
+	if ptr == nil {
+		return nil
+	}
+	v, _ = m.Load(ptr)
+	return v
+}
+
+func Unref(ptr unsafe.Pointer) {
+	if ptr == nil {
+		return
+	}
+	m.Delete(ptr)
+	C.free(ptr)
+}
