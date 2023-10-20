0d9dd7cfe20ddc4fc64649e548c4dae21d1d1c7f
aggresss <aggresss@163.com>
2023-10-20 10:44:57 +0800

    2023-10-20 10:44:57 CST W42D5

--- cgo/void/main.go
+++ cgo/void/main.go
@@ -2,6 +2,14 @@ package main
 
 /*
 #include <stdlib.h>
+#include <stdio.h>
+
+void checknull(void **v) {
+	if (!v) {
+		printf("check NULL ok\n");
+	}
+}
+
 */
 import "C"
 import (
@@ -10,8 +18,26 @@ import (
 	"unsafe"
 )
 
-func VoidPointer(a any) unsafe.Pointer {
+// CVoidPointer represents a (void*) type pointer in the C world.
+type CVoidPointer any
+
+// VoidPointer returns a unsafe.Pointer from CVoidPointer.
+func VoidPointer(a CVoidPointer) unsafe.Pointer {
+	if a == nil {
+		return unsafe.Pointer(nil)
+	}
 	return unsafe.Pointer(reflect.ValueOf(a).Pointer())
+}
+
+// CVoidPointer represents a (void**) type pointer in the C world.
+type CVoidPointerPointer any
+
+// VoidPointer returns a *unsafe.Pointer from CVoidPointerPointer.
+func VoidPointerPointer(a CVoidPointerPointer) *unsafe.Pointer {
+	if a == nil {
+		return (*unsafe.Pointer)(nil)
+	}
+	return (*unsafe.Pointer)(unsafe.Pointer(reflect.ValueOf(a).Pointer()))
 }
 
 // Not support unsafe.poiter as input
@@ -28,6 +54,10 @@ func main() {
 	fmt.Println(i)
 
 	fmt.Println(GenericPointer(&i))
+
+	fmt.Println(reflect.ValueOf(nil).Kind())
+
+	C.checknull(VoidPointerPointer(nil))
 
 	// type unsafe.Pointer of unsafe.Pointer(&i) does not match *T (cannot infer T)
 	//fmt.Println(GenericPointer(unsafe.Pointer(&i)))
