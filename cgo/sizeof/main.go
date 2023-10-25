fac7c269facca63ff1609ac4cb84fe1cc8ba6048
aggresss <aggresss@163.com>
2023-10-25 17:32:19 +0800

    2023-10-25 17:32:19 CST W43D3

--- cgo/sizeof/main.go
+++ cgo/sizeof/main.go
@@ -8,6 +8,19 @@ void c_size_of(void* ptr) {
 	printf("%lu\n", sizeof(*ptr));
 }
 
+typedef struct tou {
+	int32_t upsilon;
+	int64_t kappa;
+	int8_t phi;
+} tou;
+
+void c_size_of_tou() {
+	tou t1;
+	struct tou t2;
+	printf("%lu\n", sizeof(t1));
+	printf("%lu\n", sizeof(t2));
+}
+
 */
 import "C"
 import (
@@ -27,6 +40,14 @@ func go_size_of[T any](p *T) uintptr {
 	return unsafe.Sizeof(*p)
 }
 
+type N_TOU C.struct_tou
+
+func (n *N_TOU) GetX() int64 {
+	return int64(n.kappa)
+}
+
+type A_TOU C.tou
+
 func main() {
 	a := int32(4)
 	fmt.Println(unsafe.Sizeof(a))
@@ -37,4 +58,20 @@ func main() {
 
 	sizeofInterface(a)
 	sizeofGeneric(a)
+
+	fmt.Println("\n---")
+
+	fmt.Println(unsafe.Sizeof(N_TOU{}))
+	fmt.Println(unsafe.Sizeof(A_TOU{}))
+
+	C.c_size_of_tou()
+
 }
+
+var (
+	alphabet = []string{
+		"alpha", "bata", "gamma", "delta", "epsilon", "zeta", "eta", "theta",
+		"iota", "kappa", "lambda", "mu", "nu", "xi", "omicron", "pi",
+		"rho", "sigma", "tau", "upsilon", "phi", "chi", "psi", "omega",
+	}
+)
