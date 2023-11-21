23b9f9579ef145e2aa3608c4c1dc422bdde04425
aggresss <aggresss@163.com>
2023-11-21 11:41:17 +0800

    2023-11-21 11:41:17 CST W47D2

---
+++ cgo/integer-casting/main.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+#include <stdlib.h>
+#include <stdint.h>
+
+
+
+
+*/
+import "C"
+import "fmt"
+
+func main() {
+	i := int16(1280)
+	fmt.Println(int8(i))
+
+}
---
+++ workbench/sdl-case/go.mod
@@ -0,0 +1,5 @@
+module sdl-case
+
+go 1.21.3
+
+require github.com/veandco/go-sdl2 v0.4.35
