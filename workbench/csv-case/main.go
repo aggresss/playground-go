581be24c2e71e617ea341ee0439620fb4efe60f2
aggresss <aggresss@163.com>
2024-03-08 10:54:04 +0800

    2024-03-08 10:54:04 CST W09D5

--- workbench/csv-case/main.go
+++ workbench/csv-case/main.go
@@ -6,11 +6,16 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"time"
+)
+
+const (
+	RFC3339Milli = "2006-01-02T15:04:05.000Z07:00"
 )
 
 func main() {
 
-	f, err := os.Create("users.csv")
+	f, err := os.Create("time2.csv")
 	if err != nil {
 		log.Fatalln("failed to open file", err)
 	}
@@ -19,12 +24,30 @@ func main() {
 	w := csv.NewWriter(f)
 	defer w.Flush()
 
-	for i := 0; i < 1000; i++ {
+	if err := w.Write([]string{
+		"index",
+		"score",
+	}); err != nil {
+		log.Fatalln("error writing field record to file", err)
+	}
+
+	ticker := time.NewTicker(time.Millisecond * 1)
+	count := 0
+
+	for now := range ticker.C {
 		if err := w.Write([]string{
-			strconv.Itoa(i),
+			// strconv.FormatInt(now.UnixMilli(), 10) // grafana NOT compatibility
+			// now.Format(time.RFC3339) // grafana NOT compatibility
+			// strconv.Itoa(count) // grafana compatibility
+			// now.Format(RFC3339Milli) // grafana compatibility
+			now.Format(RFC3339Milli),
 			strconv.Itoa(rand.Intn(100)),
 		}); err != nil {
-			log.Fatalln("error writing record to file", err)
+			log.Fatalln("error writing record to file", now, err)
+		}
+		count++
+		if count > 1000 {
+			break
 		}
 	}
 }
--- workbench/time-case/main.go
+++ workbench/time-case/main.go
@@ -8,9 +8,10 @@ import (
 // Formatting / Parsing
 
 const (
-	RFC3339Mil = "2006-01-02T15:04:05.999Z07:00"
+	RFC3339Milli = "2006-01-02T15:04:05.999Z07:00"
 )
 
 func main() {
-	fmt.Println(time.Now().Format(RFC3339Mil))
+	fmt.Println(time.Now().Format(RFC3339Milli))
+	fmt.Println(time.Now().UnixMilli())
 }
